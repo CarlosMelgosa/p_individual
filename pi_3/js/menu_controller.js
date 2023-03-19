1606a2e2c62a71604f3567626e65b6d3598ff533
CarlosMelgosa <u1979254@campus.udg.edu>
2023-03-19 20:02:32 +0100

    botons de exit i de entrada creats

--- pi_3/js/menu_controller.js
+++ pi_3/js/menu_controller.js
@@ -6,11 +6,19 @@ function start_game(){
 	loadpage("./html/game.html");
 }
 
+function carrega_pi_3(){
+	loadpage("./pi_3/index.html");
+}
+
 function exit (){
 	if (name != ""){
 		alert("Leaving " + name + "'s game");
 	}
 	name = "";
+}
+
+function enrere (){
+	loadpage("../index.html");
 }
 
 function options(){
