a4c70c4ec1321c7eaa827212acb9a86f53c3a6c6
CarlosMelgosa <118018392+CarlosMelgosa@users.noreply.github.com>
2023-03-20 17:16:17 +0100

    Acabat
    Afegida la dificultat
    Afegit el canvi de temps
    Afegit el girar les cartes
    Afegit la resta de punts segons dificultad

--- pi_3/js/game_controller.js
+++ pi_3/js/game_controller.js
@@ -8,10 +8,16 @@ var game = new Vue({
 		username:'',
 		current_card: [],
 		items: [],
-		num_cards: options.getNumOfCards(),
-		bad_clicks: 0
+		num_cards: 2,
+		bad_clicks: 0,
+		comencat: false,
+		dificultat: "hard"
 	},
 	created: function(){
+		var json = localStorage.getItem("config") || '{"cards":2,"dificulty":"hard"}';
+		var game_data = JSON.parse(json);
+		this.num_cards=game_data.cards;
+		this.dificultat=game_data.dificulty;
 		this.username = sessionStorage.getItem("username","unknown");
 		this.items = items.slice(); // Copiem l'array
 		this.items.sort(function(){return Math.random() - 0.5}); // Array aleatòria
@@ -19,8 +25,24 @@ var game = new Vue({
 		this.items = this.items.concat(this.items); // Dupliquem els elements
 		this.items.sort(function(){return Math.random() - 0.5}); // Array aleatòria
 		for (var i = 0; i < this.items.length; i++){
-			this.current_card.push({done: false, texture: back});
+			this.current_card.push({done: false, texture: this.items[i]});
 		}
+		var tempor=0;
+		if (game_data.dificulty=="hard"){
+			tempor=300;
+		}
+		else if (game_data.dificulty=="normal"){
+			tempor=1000;
+		}
+		else {
+			tempor=2000;
+		}
+		setTimeout(() => {
+			for (var i = 0; i < this.items.length; i++){
+				Vue.set(this.current_card, i, {done: false, texture: back});
+			}
+			this.comencat=true;
+		}, tempor);
 	},
 	methods: {
 		clickCard: function(i){
@@ -29,35 +51,47 @@ var game = new Vue({
 		}
 	},
 	watch: {
-		current_card: function(value){
-			if (value.texture === back) return;
-			var front = null;
-			var i_front = -1;
-			for (var i = 0; i < this.current_card.length; i++){
-				if (!this.current_card[i].done && this.current_card[i].texture !== back){
-					if (front){
-						if (front.texture === this.current_card[i].texture){
-							front.done = this.current_card[i].done = true;
-							this.num_cards--;
-						}
-						else{
-							Vue.set(this.current_card, i, {done: false, texture: back});
-							Vue.set(this.current_card, i_front, {done: false, texture: back});
-							this.bad_clicks++;
-							break;
+			current_card: function(value){
+				if (this.comencat == true){
+					if (value.texture === back) return;
+					var front = null;
+					var i_front = -1;
+					for (var i = 0; i < this.current_card.length; i++){
+						if (!this.current_card[i].done && this.current_card[i].texture !== back){
+							if (front){
+								if (front.texture === this.current_card[i].texture){
+									front.done = this.current_card[i].done = true;
+									this.num_cards--;
+								}
+								else{
+									Vue.set(this.current_card, i, {done: false, texture: back});
+									Vue.set(this.current_card, i_front, {done: false, texture: back});
+									this.bad_clicks++;
+									break;
+								}
+							}
+							else{
+								front = this.current_card[i];
+								i_front = i;
+							}
 						}
 					}
-					else{
-						front = this.current_card[i];
-						i_front = i;
-					}
-				}
-			}			
-		}
+				}			
+			}
 	},
 	computed: {
 		score_text: function(){
-			return 100 - this.bad_clicks * 20;
+			var resta=20;
+			if (this.dificultat=="hard"){
+				resta=50;
+			}
+			else if (this.dificultat=="normal"){
+				resta=15;
+			}
+			else {
+				resta=10;
+			}
+			return 100 - this.bad_clicks * resta;
 		}
 	}
 });
